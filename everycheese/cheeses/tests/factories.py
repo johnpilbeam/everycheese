2d2daf4fa6c35a16f3fa292281fef7e304ade1a9
johnpilbeam <jpilbeam@gmail.com>
2020-08-23 23:18:14 +0100

    Write tests for all cheese URL patterns

--- everycheese/cheeses/tests/factories.py
+++ everycheese/cheeses/tests/factories.py
@@ -1,4 +1,5 @@
 from django.template.defaultfilters import slugify
+from everycheese.users.tests.factories import UserFactory
 
 import factory 
 import factory.fuzzy
@@ -15,8 +16,9 @@ class CheeseFactory(factory.django.DjangoModelFactory):
     )
     firmness = factory.fuzzy.FuzzyChoice( 
         [x[0] for x in Cheese.Firmness.choices]
-    country_of_origin = factory.Faker('country_code')
     )
+    country_of_origin = factory.Faker('country_code')
+    creator = factory.SubFactory(UserFactory)
     
     class Meta:
         model = Cheese
